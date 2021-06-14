77c93218290a07cf3b6fdc113dff9dd9b24ba257
TommyWU-0416 <49664867+TommyWU-0416@users.noreply.github.com>
2021-06-14 16:31:58 +0800

    http://web.ntnu.edu.tw/~algo/DynamicProgramming.html#2 實作

--- LeetCodeEX/src/com/leetcode/challenge/june_21/Interleaving_String.java
+++ LeetCodeEX/src/com/leetcode/challenge/june_21/Interleaving_String.java
@@ -14,9 +14,12 @@ package com.leetcode.challenge.june_21;
  *        Output: false
  */
 public class Interleaving_String {
-	private static String s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac";
+//	private static String s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac";
 //	private static String s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc";
+//	private static String s1 = "a", s2 = "", s3 = "c";
+//	private static String s1 = "", s2 = "", s3 = "";
 //	private static String s1 = "ab", s2 = "cd", s3 = "abcd";
+	private static String s1 = "aa", s2 = "ab", s3 = "aaba"; /* true */
 
 	public static void main(String[] args) {
 		Interleaving_String test = new Interleaving_String();
@@ -24,22 +27,25 @@ public class Interleaving_String {
 		System.out.println("boolean:" + b);
 	}
 
-	public boolean is_Interleave(String s1, int i, String s2, int j, String res, String s3)
-    {
-        if(res.equals(s3) && i == s1.length() && j == s2.length())
-            return true;
-        boolean ans = false;
-        if(i < s1.length())
-            ans |= is_Interleave(s1, i + 1, s2, j, res + s1.charAt(i), s3);
-        if(j < s2.length())
-            ans |= is_Interleave(s1, i, s2, j + 1, res + s2.charAt(j), s3);
-        return ans;
-
-    }
-    public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1.length() + s2.length() != s3.length()) {
-            return false;
-        }
-        return is_Interleave(s1, 0, s2, 0, "", s3);
-    }
+	public boolean isInterleave(String s1, String s2, String s3) {
+		if (s3.length() != s1.length() + s2.length()) {
+			return false;
+		}
+		boolean dp[][] = new boolean[s1.length() + 1][s2.length() + 1];
+		for (int i = 0; i <= s1.length(); i++) {
+			for (int j = 0; j <= s2.length(); j++) {
+				if (i == 0 && j == 0) {
+					dp[i][j] = true;
+				} else if (i == 0) {
+					dp[i][j] = dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+				} else if (j == 0) {
+					dp[i][j] = dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+				} else {
+					dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1))
+							|| (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+				}
+			}
+		}
+		return dp[s1.length()][s2.length()];
+	}
 }
